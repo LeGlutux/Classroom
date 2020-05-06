b142addde57d85549cac966238f0c81c08383c30
LeGlutux <lp.bendeks@gmail.com>
2020-05-06 21:47:57 +0200

    Revert "Revert "student add does not work anymore""
    This reverts commit 74c8de9474b0e4fdcad1b26f6c3b3b5f8082a86f.

--- src/components/Create/CreateStudent.tsx
+++ src/components/Create/CreateStudent.tsx
@@ -47,9 +47,10 @@ export default ({ groups }: Props) => {
                                 classes: list,
                                 id,
                             })
-                        firestore.collection('paths').doc(id).set({ id })
                         setNameInputValue('')
                         setSurnameInputValue('')
+                        e.preventDefault()
+                    e.stopPropagation()
                     } else {
                         throw new Error("le formulaire n'est pas complet")
                     }
@@ -68,12 +69,12 @@ export default ({ groups }: Props) => {
                                 onChange={(e) =>
                                     setSurnameInputValue(e.target.value)
                                 }
-                                className="h-10 mt-3 placeholder-gray-700 ml-5 bg-transparent border-b-2 border-gray-600 text-xl"
+                                className="h-10 mt-3 placeholder-gray-700 ml-5 bg-transparent border-b-2 border-gray-600 text-lg"
                                 type="text"
                                 placeholder="Prénom de l'élève"
                             />
                             <input
-                                className="h-10 mt-3 placeholder-gray-700 text-xl ml-5 bg-transparent border-b-2 border-gray-600"
+                                className="h-10 mt-3 placeholder-gray-700 text-lg ml-5 bg-transparent border-b-2 border-gray-600"
                                 value={nameInputValue}
                                 onChange={(e) =>
                                     setNameInputValue(e.target.value)
@@ -97,7 +98,7 @@ export default ({ groups }: Props) => {
                     </div>
                     <button
                         type="submit"
-                        className="flex h-10 w-48 self-center mt-6 bg-orange-500 rounded text-white flex text-xl font-bold justify-center"
+                        className="flex h-8 w-40 self-center mt-6 bg-orange-500 rounded text-white flex text-lg font-bold justify-center"
                     >
                         Ajouter l'élève
                     </button>
