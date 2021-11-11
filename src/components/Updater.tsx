879f6497bf1b91418756c4998e9d61b93a6a9191
LeGlutux <lp.bendeks@gmail.com>
2021-11-11 19:13:25 +0100

    deploy

--- src/components/Updater.tsx
+++ src/components/Updater.tsx
@@ -46,7 +46,7 @@ export default (props: UpdaterProps) => {
                     updateRequired ? 'entering-t' : 'invisible'
                 }`}
             >
-                <span className="absolute top-0 right-0 p-4">
+                {/* <span className="absolute top-0 right-0 p-4">
                     <svg
                         className="h-4 w-4 fill-current text-grey hover:text-grey-darkest"
                         role="button"
@@ -55,9 +55,16 @@ export default (props: UpdaterProps) => {
                         <title>Close</title>
                         <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
                     </svg>
-                </span>
+                </span> */}
                 <div className="text-xl text-center font-bold mx-2">
                     Votre version de Thòt Note n'est pas à jour{' '}
+                </div>
+
+                <div className="text-sm text-center mx-2 mt-4">
+                    La prochaine version propose des icônes pour les croix
+                    personnalisables, vous pourrez également en ajouter (jusqu'à
+                    6) ou en retirer. Pas d'inquiétude, retirer les icônes ne
+                    fait pas disparaître les données.{' '}
                 </div>
 
                 <div className="flex flex-col h-40 justify-around mt-12 items-center">
